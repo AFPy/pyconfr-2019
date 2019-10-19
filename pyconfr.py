a19dbec5a8886807e7f74d4be94d0dc4acc7be84
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2019-10-20 00:12:24 +0200

    Many small fixes for talks anchors

--- pyconfr.py
+++ pyconfr.py
@@ -1,8 +1,9 @@
+from urllib.parse import quote
 from urllib.request import urlopen
 from xml.etree import ElementTree
 
 from bs4 import BeautifulSoup
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_frozen import Freezer
 from markdown2 import Markdown
 from sassutils.wsgi import SassMiddleware
@@ -14,6 +15,11 @@ app.wsgi_app = SassMiddleware(app.wsgi_app, {
         'css_path': 'static/css',
         'wsgi_path': '/2019/static/css',
         'strip_extension': True}})
+
+
+@app.template_filter()
+def slug(string):
+    return quote(str(string).lower(), safe='')
 
 
 @app.route('/')
@@ -47,18 +53,36 @@ def talks(lang, category):
         category=category, talks=talks, lang=lang)
 
 
-@app.route('/2019/schedule.html')
-def schedule():
+@app.route('/2019/<lang>/full-schedule.html')
+def schedule(lang):
     with urlopen('https://cfp-2019.pycon.fr/schedule/html/') as fd:
-        data = fd.read().decode('utf-8')
-        # Delete extra cells for sprints
-        data = (
-            data
-            .replace('colspan="9"', '')
-            .replace('<td colspan="8"></td>', ''))
+        html = fd.read().decode('utf-8')
 
-    # insert link in the table
-    soup = BeautifulSoup(data, 'html.parser')
+    if lang == 'fr':
+        html = html.replace('Room', 'Salle')
+    else:
+        html = (
+            html
+            .replace('samedi 02 novembre', 'Saturday, November 2')
+            .replace('dimanche 03 novembre', 'Sunday, November 3'))
+        for minute in (0, 30):
+            html = html.replace(f'12:{minute:02}', f'12:{minute:02} PM')
+            for hour in range(9, 12):
+                html = html.replace(
+                    f'{hour:02}:{minute:02}', f'{hour:02}:{minute:02} AM')
+            for hour in range(13, 19):
+                html = html.replace(
+                    f'{hour:02}:{minute:02}',
+                    f'{hour-12:02}:{minute:02} PM')
+
+    # Delete extra cells for sprints
+    html = (
+        html
+        .replace('colspan="9"', '')
+        .replace('<td colspan="8"></td>', ''))
+
+    # Insert links in the table
+    soup = BeautifulSoup(html, 'html.parser')
     conf_colors = {
         '#ff7373': 'keynote',
         '#73cbef': 'workshop',
@@ -67,14 +91,12 @@ def schedule():
     for color, kind in conf_colors.items():
         for td in soup.find_all('td', attrs={'bgcolor': color}):
             title = list(td.children)[0]
-            href = '/2019/fr/talks/{}.html#{}'.format(
-                kind, str(title).lower()
-            )
-            link = soup.new_tag('a', href=href)
+            url = url_for('talks', lang=lang, category=kind)
+            href = f'{url}#{slug(title)}'
+            link = soup.new_tag('a', href=href, target='_parent')
             title.wrap(link)
-    data = str(soup)
 
-    return render_template('schedule.html.jinja2', data=data)
+    return render_template('schedule.html.jinja2', data=soup)
 
 
 @app.cli.command('freeze')
